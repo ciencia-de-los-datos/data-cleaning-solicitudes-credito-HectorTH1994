e2d93dd0661e13e57d60e2ec11c24a20b5c4591b
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-10-16 19:42:05 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,6 +7,7 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
+ 
 
 
 def clean_data():
@@ -17,4 +18,53 @@ def clean_data():
     # Inserte su código aquí
     #
 
+    df=df[df.columns[1:]]
+    #df.drop_duplicates(inplace=True)
+    #df.dropna(inplace=True)
+    
+    #Colocamos sexo en minusculas y eliminamos espacios en blanco
+    df.sexo=df.sexo.str.lower()
+
+    #para tipo de tipo_de_emprendimiento
+    df.tipo_de_emprendimiento=df.tipo_de_emprendimiento.str.lower()
+    df.tipo_de_emprendimiento=df.tipo_de_emprendimiento.str.strip()
+
+
+    #para tipo de idea_negocio
+    df.idea_negocio=df.idea_negocio.str.replace('-',' ')
+    df.idea_negocio=df.idea_negocio.str.replace('_',' ')
+    df.idea_negocio=df.idea_negocio.str.lower()
+    df.idea_negocio=df.idea_negocio.str.strip()
+
+    #Barrio
+    df.barrio=df.barrio.str.replace('-',' ')
+    df.barrio=df.barrio.str.replace('_',' ')
+    df.barrio=df.barrio.str.replace('.',' ')
+    df.barrio=df.barrio.str.lower()
+    df.barrio=df.barrio.str.strip()
+
+    #linea de credito
+    df.línea_credito=df.línea_credito.str.replace('-',' ')
+    df.línea_credito=df.línea_credito.str.replace('_',' ')
+    df.línea_credito=df.línea_credito.str.replace('.','')
+    df.línea_credito=df.línea_credito.str.lower()
+    df.línea_credito=df.línea_credito.str.strip()
+
+    #fecha
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+    #print(df.monto_del_credito.value_counts())
+    #Monto de credito
+    df.monto_del_credito=df.monto_del_credito.str.replace(',','.')
+    df.monto_del_credito=df.monto_del_credito.str.replace('$','')
+    df.monto_del_credito=df.monto_del_credito.str.strip()
+    df.monto_del_credito=df.monto_del_credito.apply(pd.to_numeric, downcast='integer', errors='ignore')
+    #print(df[df.estrato==0])
+    df.drop_duplicates(inplace=True)
+    df.dropna(inplace=True)
+    
+    print(df.estrato.value_counts())
+    #print(df.comuna_ciudadano.value_counts())
+    #print(df.sexo.value_counts().to_list())
+
     return df
+    
